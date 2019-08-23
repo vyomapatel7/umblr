b27d2ff8d8258d12a36c7f9d0e678b5008854d05
vyomapatel7 <vyoma.patel07@gmail.com>
2019-08-23 13:54:39 +0200

    Created a post model

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Blog
+from .models import Blog, Post
 
 admin.site.register(Blog)
+admin.site.register(Post)
--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,10 @@ class Blog(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True
     )
+
+class Post(models.Model):
+    postTitle = models.CharField(max_length=200, blank=False)
+    postText = models.CharField(max_length=1000, blank=True)
+    blog = models.ForeignKey(
+        Blog, on_delete=models.CASCADE, blank=True, null=True
+    )
