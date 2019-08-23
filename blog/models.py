bc14f5a5cf4a0717998755b04f0b4d2c51e804dc
vyomapatel7 <vyoma.patel07@gmail.com>
2019-08-23 13:44:33 +0200

    Created a blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Blog(models.Model):
+    blogTitle = models.CharField(max_length=200, blank=False)
+    blogBio = models.CharField(max_length=500, blank=True)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True
+    )
