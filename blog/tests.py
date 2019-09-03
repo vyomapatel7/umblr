8aaeda62b612b8a082cf795d26ad45cc48b326a2
vyomapatel7 <vyoma.patel07@gmail.com>
2019-09-03 14:33:19 +0200

    Unittest Home returns correct html

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import resolve
 from blog.views import home
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
@@ -9,3 +11,9 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home)
 
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home(request)
+        html = response.content.decode('utf8')
+        expected_html = render_to_string('home.html')
+        self.assertEqual(html, expected_html)
