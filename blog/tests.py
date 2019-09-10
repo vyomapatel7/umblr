623fc802d077eb513e509a1b428e775d79648a7c
vyomapatel7 <vyoma.patel07@gmail.com>
2019-09-10 10:57:08 +0200

    Added to HomePageTest

--- blog/tests.py
+++ blog/tests.py
@@ -9,6 +9,10 @@ from .models import Blog
 
 class HomePageTest(TestCase):
 
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home)
